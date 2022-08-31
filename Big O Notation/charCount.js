b334f00a6ab1a5b0dd9a7a6fb9b3177ac8264a26
Hailey_ys <whassup_man@naver.com>
2022-08-31 09:54:33 +0900

    Add charCount

---
+++ Big O Notation/charCount.js
@@ -0,0 +1,13 @@
+function charCount(str) {
+  var obj = {};
+  for (var i = 0; i < str.length; i++) {
+    var char = str[i].toLowerCase();
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  }
+}

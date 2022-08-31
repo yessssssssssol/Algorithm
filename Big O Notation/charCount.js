69e3e2e13eaf98010d80f6ade0841199d8672bac
Hailey_ys <whassup_man@naver.com>
2022-08-31 14:57:55 +0900

    Add commit

--- Big O Notation/charCount.js
+++ Big O Notation/charCount.js
@@ -2,6 +2,7 @@ function charCount(str) {
   var obj = {};
   for (var i = 0; i < str.length; i++) {
     var char = str[i].toLowerCase();
+    //a~z 또는 0~9가 한 번 이상 반복되는 문자열을 전역 검색
     if (/[a-z0-9]/.test(char)) {
       if (obj[char] > 0) {
         obj[char]++;

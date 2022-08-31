2f308af0a81f48d7b8fa73b23824e54bc3fb41dd
Hailey_ys <whassup_man@naver.com>
2022-08-31 16:33:01 +0900

    Add charCount2?

--- Big O Notation/charCount.js
+++ Big O Notation/charCount.js
@@ -11,4 +11,23 @@ function charCount(str) {
       }
     }
   }
+  return obj;
 }
+
+function charCount2(str) {
+  var obj = {};
+  for (var char of str) {
+    var char = char.toLowerCase();
+    //a~z 또는 0~9가 한 번 이상 반복되는 문자열을 전역 검색 = /[\w]
+    if (/[a-z0-9]/.test(char)) {
+      if (obj[char] > 0) {
+        obj[char]++;
+      } else {
+        obj[char] = 1;
+      }
+    }
+  }
+  return obj;
+}
+
+console.log(charCount2('Hello hi!'));

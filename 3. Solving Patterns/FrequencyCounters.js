d7f1d49d723e0eb5abcf3acd417fa49d97734b7d
Hailey_ys <whassup_man@naver.com>
2022-09-03 04:54:30 +0900

    Add same funciton

--- 3. Solving Patterns/FrequencyCounters.js
+++ 3. Solving Patterns/FrequencyCounters.js
@@ -1,10 +1,5 @@
 // 2개의 배열을 허용하는 same이라는 함수를 작성하십시오. 첫번째 배열의 제곱이 두 번째 배열에 해당하는 값을 가지면 참을 반환
 
-//example
-Same([1, 2, 3], [4, 1, 9]); //true
-Same([1, 2, 3], [1, 9]); //false
-Same([1, 2, 1], [4, 4, 1]); //false (must be same frequency)
-
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -18,3 +13,8 @@ function same(arr1, arr2) {
   }
   return true;
 }
+
+//example
+same([1, 2, 3], [4, 1, 9]); //true
+same([1, 2, 3], [1, 9]); //false
+same([1, 2, 1], [4, 4, 1]); //false (must be same frequency)

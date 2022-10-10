4974e8abf40ee2062fbf2bd48943478106d8d86a
Hailey_ys <whassup_man@naver.com>
2022-10-10 22:24:23 +0900

    Add basic-6

--- JS Basic/6.js
+++ JS Basic/6.js
@@ -1,8 +1,18 @@
 // 홀수
 
 function solution(arr) {
-  let answer;
+  let answer = [];
+  let sum = 0,
+    min = 987654321;
 
+  for (let x of arr) {
+    if (x % 2 === 1) {
+      sum += x;
+      if (x < min) min = x;
+    }
+  }
+  answer.push(sum);
+  answer.push(min);
   return answer;
 }
 arr = [12, 77, 38, 41, 53, 92, 85];

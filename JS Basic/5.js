8e63a685e814da64fbedbb89c1f5706e98eff5e8
Hailey_ys <whassup_man@naver.com>
2022-10-10 22:13:02 +0900

    Add basic-5 comment

--- JS Basic/5.js
+++ JS Basic/5.js
@@ -12,3 +12,17 @@ function solution(arr) {
 
 arr = [5, 3, 7, 11, 9, 15, 17];
 console.log(solution(arr));
+
+function solution2(arr) {
+  let answer,
+    min = 987654321; //min값 미리 주는 것 잊지 않기
+  for (let i = 0; i <= arr.length; i++) {
+    if (arr[i] < min) min = arr[i];
+    answer = min;
+  }
+
+  return answer;
+}
+arr2 = [5, 7, 3, 11, 2, 15, 17];
+
+console.log(solution2(arr2));

811f7995a5ddc1dcbda39d5c9314c09c33161a0c
Hailey_ys <whassup_man@naver.com>
2022-10-10 22:07:37 +0900

    Add basic-4 comment

--- JS Basic/4.js
+++ JS Basic/4.js
@@ -1,7 +1,7 @@
 // 1부터 N까지 합 출력하기
 
 function solution(n) {
-  let answer = 0;
+  let answer = 0; // 초기값 0으로 설정하기, 하지 않으면 NaN로 뜸
   for (let i = 1; i <= n; i++) {
     console.log(i);
     answer = answer + i;

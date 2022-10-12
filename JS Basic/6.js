8da5b4d55fd71216f3f13cbd077c836c25ef301c
Hailey_ys <whassup_man@naver.com>
2022-10-12 14:17:12 +0900

    Add basic-6

--- JS Basic/6.js
+++ JS Basic/6.js
@@ -17,3 +17,23 @@ function solution(arr) {
 }
 arr = [12, 77, 38, 41, 53, 92, 85];
 console.log(solution(arr));
+
+function solution2(arr2) {
+  let answer = [];
+  let sum2 = 0,
+    min2 = 987654321;
+  //min = Number.MAX_SAFE_INTEGER
+
+  for (let x of arr2) {
+    if (x % 2 === 1) {
+      sum2 = sum2 + x;
+      if (x < min2) min2 = x;
+    }
+  }
+  answer.push(sum2);
+  answer.push(min2);
+  return answer;
+}
+
+arr2 = [12, 77, 38, 41, 53, 92, 85];
+console.log(solution2(arr2));
